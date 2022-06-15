24c33482fda30e5c8699b9baf58289b12a70b4a9
oyiadin <oyiadin@gmail.com>
2022-06-16 00:00:54 +0800

    [chore] add test data

--- main.py
+++ main.py
@@ -3,6 +3,24 @@
 from minesqlite.minesqlite import MineSQLite
 from minesqlite.repl import repl_loop
 
+"""
+Test data:
+
+add id 222 name "name BBB" in_date 2022-07-06 department P6 position "SDE4"
+add id 111 name "name HHH" in_date 2022-05-05 department P2 position "SDE7"
+add id 777 name "name CCC" in_date 2022-06-05 department P1 position "SDE2"
+add id 666 name "name GGG" in_date 2022-08-05 department P1 position "SDE6"
+add id 555 name "name EEE" in_date 2022-04-05 department P1 position "SDE6"
+add id 888 name "name DDD" in_date 2022-02-05 department P5 position "SDE5"
+add id 333 name "name AAA" in_date 2022-07-05 department P1 position "SDE6"
+add id 444 name "name FFF" in_date 2022-01-05 department P3 position "SDE1"
+
+list
+list department P1
+list department P1 $sort_desc position
+list department P1 $sort_desc position $sort_asc name
+"""
+
 if __name__ == '__main__':
     instance = MineSQLite(
         sysconf_kwargs={'conf_file': open('etc/config.ini')})
